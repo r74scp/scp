8e7de1c34927ec39b94a40fd0b714aae70351723
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2021-07-11 17:18:12 +0900

    Update main.py

--- tags/main.py
+++ tags/main.py
@@ -1,6 +1,6 @@
 import requests
 r = requests.get("https://rtas.wdfiles.com/local--code_/gas%3Ataglistjp/1")
 print(r.text)
-f = open('../tags/tags.md', 'w')
+f = open('/tags/tags.md', 'w')
 f.write(r.text)
 f.close()
