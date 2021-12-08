8e7264e9e25635ddad5c92b207f45e42d5c40829
RTa-technology <scpfoundationidcardmaker@gmail.com>
2021-12-09 00:11:37 +0900

    fix: directory

--- tags/tags.py
+++ tags/tags.py
@@ -1,10 +1,10 @@
 import re
 
-f = open("tags.md", "r", encoding="utf-8_sig")
+f = open("./tags/tags.md", "r", encoding="utf-8_sig")
 data = f.read()
 f.close()
 
-f = open('tags_commented.md', 'w', encoding="utf-8_sig")
+f = open('./tags/tags_commented.md', 'w', encoding="utf-8_sig")
 re_data = re.sub(r'(\n)?\[!\-\-(.*?)\-\-\]', '', data, flags=(re.IGNORECASE | re.DOTALL))
 f.write(re_data)
 f.close()
