5b5fcb7aa9f7babcd571e3b7a54c2c4c4bbb184a
RTa-technology <scpfoundationidcardmaker@gmail.com>
2021-12-15 16:17:20 +0900

    feat: add EN list

---
+++ scp-wiki/tags/fetch.py
@@ -0,0 +1,6 @@
+import requests
+r = requests.get("https://rtas.wdfiles.com/local--code_/gas%3Ataglisten/1")
+print(r.text)
+f = open('./scp-wiki/tags/tags.md', 'w', encoding="utf-8_sig")
+f.write(r.text)
+f.close()
--- scp-wiki/tags/non-comment.py
+++ scp-wiki/tags/non-comment.py
@@ -1,11 +1,10 @@
 import re
 
-f = open("./scp-jp"
-         "tags/tags.md", "r", encoding="utf-8_sig")
+f = open("./scp-wiki/tags/tags.md", "r", encoding="utf-8_sig")
 data = f.read()
 f.close()
 
-f = open('./scp-jp/tags/tags_commented.md', 'w', encoding="utf-8_sig")
+f = open('./scp-wiki/tags/tags_commented.md', 'w', encoding="utf-8_sig")
 re_data = re.sub(r'(\n)?\[!\-\-(.*?)\-\-\]', '', data, flags=(re.IGNORECASE | re.DOTALL))
 f.write(re_data)
 f.close()
